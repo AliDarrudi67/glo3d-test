7e38d34eb07e5df0e98eab83dfa007c1f4823aab
darrudi <ali.daroudi@gmail.com>
2023-04-26 10:49:22 +0430

    project created and material angular added to project

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MediaComponent } from './components/media/media.component';
+import { ServicesComponent } from './services/services.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MediaComponent,
+    ServicesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
